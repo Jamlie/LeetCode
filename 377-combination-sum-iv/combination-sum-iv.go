dab4d80c7056aae1b19df530bd52bba365ed0e7d
Omar <88511754+Jamlie@users.noreply.github.com>
2024-08-10 17:57:21 +0300

    Time: 2 ms (24.72%) | Memory: 2.3 MB (19.10%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.go
@@ -0,0 +1,26 @@
+func combinationSum4(nums []int, target int) int {
+	return findCombinations(nums, target, make(map[int]int))
+}
+
+func findCombinations(nums []int, target int, memo map[int]int) int {
+	if target == 0 {
+		return 1
+	}
+
+	if target < 0 {
+		return 0
+	}
+
+	if v, ok := memo[target]; ok {
+		return v
+	}
+
+	result := 0
+	for _, num := range nums {
+		result += findCombinations(nums, target-num, memo)
+	}
+
+    memo[target] = result
+
+	return result
+}
