3031ca929a2f675ab211c859228f4d65113d5a9f
Omar <88511754+Jamlie@users.noreply.github.com>
2024-08-15 16:52:01 +0300

    Time: 12 ms (43.52%) | Memory: 6.1 MB (23.87%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -0,0 +1,35 @@
+type Coordinates struct {
+	row int
+	col int
+}
+
+func setZeroes(matrix [][]int) {
+	if len(matrix) == 0 {
+		return
+	}
+
+	zeroesLocations := []Coordinates{}
+	n := len(matrix)
+	m := len(matrix[0])
+
+	for i := range n {
+		for j := range m {
+			if matrix[i][j] == 0 {
+				zeroesLocations = append(zeroesLocations, Coordinates{
+					row: i,
+					col: j,
+				})
+			}
+		}
+	}
+
+	for _, location := range zeroesLocations {
+		for i := range n {
+			matrix[i][location.col] = 0
+		}
+
+		for j := range m {
+			matrix[location.row][j] = 0
+		}
+	}
+}
